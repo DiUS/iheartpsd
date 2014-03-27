d1422197ccdf8f5f9e74e57b90f5247551afafe1
kohlerfc <c.kohler.aust@gmail.com>
2014-03-28 10:30:38 +1100

    Added upload page

--- app.rb
+++ app.rb
@@ -1,19 +1,24 @@
 require 'sinatra'
 require 'slim'
+require 'haml'
 require 'psd'
 require 'json'
 
 class App < Sinatra::Base
 
+# Handle GET-request (Show the upload form)
   get '/upload' do
-     haml :upload
+    haml :upload
   end
 
+# Handle POST-request (Receive and save the uploaded file)
   post '/upload' do
-    unless params[:file] && (tmpfile = params[:file][:tempfile]) && (name = params[:file][:filename])
-      return haml(:upload)
+    File.open('uploads/' + params['myfile'][:filename], "w") do |f|
+      f.write(params['myfile'][:tempfile].read)
     end
+    redirect '/process/' + /myfile
   end
+
 
   get '/slim_example' do
     slim :slim_example
